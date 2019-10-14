1bfc5eefe1f3143a48a9cb6b408876c8bceec152
sbroad <sara.broad@aixplatform.com>
2019-10-13 20:05:19 -0400

    add knex

---
+++ db/migrations/01_books.js
@@ -0,0 +1,18 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('Books', function (table) {
+            table.increments('id');
+            table.string('title').notNullable();
+            table.string('author').notNullable();
+            table.date('dateFinished').notNullable();
+            table.integer('pages').notNullable();
+            table.integer('rating').notNullable();
+        });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTable("books")
+}
+
+exports.config = { transaction: false }
---
+++ db/seeds/01_books.js
@@ -0,0 +1,77 @@
+exports.seed = function (knex) {
+    return knex('Books').del()
+        .then(function () {
+            return knex('Books').insert([{
+                    title: 'Heavy',
+                    author: 'Kiese Laymon',
+                    datefinished: '2019-01-21',
+                    pages: 256,
+                    rating: 5
+                },
+                {
+                    id: 2,
+                    title: 'Boy Swallows Universe',
+                    author: 'Trent Dalton',
+                    datefinished: '2019-04-27',
+                    pages: 464,
+                    rating: 5
+                },
+                {
+                    id: 3,
+                    title: 'The Buried',
+                    author: 'Peter Hessler',
+                    datefinished: '2019-05-22',
+                    pages: 480,
+                    rating: 5
+                },
+                {
+                    id: 4,
+                    title: 'The Body Papers',
+                    author: 'Grace Talusan',
+                    datefinished: '2019-07-17',
+                    pages: 256,
+                    rating: 5
+                },
+                {
+                    id: 6,
+                    title: 'Small Fry',
+                    author: 'Lisa Jobs',
+                    datefinished: '2018-11-18',
+                    pages: 383,
+                    rating: 4
+                },
+                {
+                    id: 7,
+                    title: 'Boom Town',
+                    author: 'Sam Anderson',
+                    datefinished: '2018-09-22',
+                    pages: 448,
+                    rating: 5
+                },
+                {
+                    id: 8,
+                    title: 'Flights',
+                    author: 'Olga Tokarczuk',
+                    datefinished: '2018-10-18',
+                    pages: 403,
+                    rating: 3
+                },
+                {
+                    id: 9,
+                    title: 'The Hearts Invisble Furies',
+                    author: 'John Boyne',
+                    datefinished: '2019-08-18',
+                    pages: 582,
+                    rating: 5
+                },
+                {
+                    id: 10,
+                    title: 'The Great Believers',
+                    author: 'Rebecca Mekkai',
+                    datefinished: '2019-01-01',
+                    pages: 421,
+                    rating: 3
+                },
+            ])
+        })
+}
