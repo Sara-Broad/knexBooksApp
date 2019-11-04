dae325b43528349f10579c9af83dd3a376f9ab09
sbroad <sara.broad@aixplatform.com>
2019-11-03 21:14:14 -0500

    adding tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,37 +2,6 @@ const db = require('../db/knex')
 const app = require('../server')
 const request = require('supertest')
 
-
-
-// describe('outer', () => {
-//     console.log('describe outer-a');
-  
-//     describe('describe inner 1', () => {
-//       console.log('describe inner 1');
-//       test('test 1', () => {
-//         console.log('test for describe inner 1');
-//         expect(true).toEqual(true);
-//       });
-//     });
-  
-//     console.log('describe outer-b');
-  
-//     test('test 1', () => {
-//       console.log('test for describe outer');
-//       expect(true).toEqual(true);
-//     });
-  
-//     describe('describe inner 2', () => {
-//       console.log('describe inner 2');
-//       test('test for describe inner 2', () => {
-//         console.log('test for describe inner 2');
-//         expect(false).toEqual(false);
-//       });
-//     });
-  
-//     console.log('describe outer-c');
-//   });
-
 describe('GET /books', () => {
     beforeEach(async () => {
         await db('Books').truncate();
